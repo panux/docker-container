b332904720718da369e36ac9949565aa0f341c4a
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-03 15:50:26 -0400

    fix key setup

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ packages=(basefs busybox musl libgpg-error libassuan libgcrypt zlib libintl gnup
 repo="https://repo.projectpanux.com/beta"
 
 gpg --keyserver pgp.mit.edu --recv-keys DD8203F5 || { echo "error fetching pgp key"; exit 1; }
-expect -c "spawn gpg --edit-key DD8203F5 0 --yes trust quit; expect \"Your decision? \" { send \"5\r\" }; expect \"Do you really want to set this key to ultimate trust? (y/N) \" { send \"y\r\" }; interact"
+expect -c "spawn gpg --edit-key DD8203F5 trust quit; expect \"Your decision? \" { send \"5\r\" }; expect \"Do you really want to set this key to ultimate trust? (y/N) \" { send \"y\r\" }; interact"
+cp ~/.gnupg/pubring.gpg ~/.gnupg/trustedkeys.gpg
 
 function fetchpackage() {
 	wget $repo/pkgs/$pkg.tar.xz -O packages/$pkg.tar.xz
