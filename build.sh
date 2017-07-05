84d11bc3c39cc5b202055fab479fcac1196047da
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-05 10:56:43 -0400

    switch over to lpkg [skip ci]

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@ gpg --keyserver pgp.mit.edu --recv-keys DD8203F5 || { echo "error fetching pgp k
 expect -c "spawn gpg --edit-key DD8203F5 trust quit; expect \"Your decision? \" { send \"5\r\" }; expect \"Do you really want to set this key to ultimate trust? (y/N) \" { send \"y\r\" }; interact"
 cp ~/.gnupg/pubring.gpg ~/.gnupg/trustedkeys.gpg
 
-git clone https://github.com/panux/shpkg.git
+git clone https://github.com/panux/lpkg.git
 mkdir build
-export ROOTFS=$(pwd)/build
-export SHPKG="/bin/bash $PWD/shpkg/shpkg.sh"
 echo $ROOTFS
-chmod 700 shpkg/shpkg.sh
-/bin/bash shpkg/shpkg.sh bootstrap $(pwd)/shpkg
+chmod 700 lpkg/lpkg.sh
+lpkg/lpkg.lua bootstrap $(pwd)/build https://repo.projectpanux.com/beta/pkgs/
 
 docker build -t panux/panux .
