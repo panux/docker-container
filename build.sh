3ab50b549d800ae7827d40fc35bb80192662d1ff
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-05 11:10:56 -0400

    Change keyserver to keys.gnupg.net

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gpg --keyserver pgp.mit.edu --recv-keys DD8203F5 || { echo "error fetching pgp key"; exit 1; }
+gpg --keyserver keys.gnupg.net --recv-keys DD8203F5 || { echo "error fetching pgp key"; exit 1; }
 expect -c "spawn gpg --edit-key DD8203F5 trust quit; expect \"Your decision? \" { send \"5\r\" }; expect \"Do you really want to set this key to ultimate trust? (y/N) \" { send \"y\r\" }; interact"
 cp ~/.gnupg/pubring.gpg ~/.gnupg/trustedkeys.gpg
 
