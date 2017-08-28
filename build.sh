047162cb672d51900eedb32eadf15e333653811a
Jaden Weiss <jadr2ddude@gmail.com>
2017-08-27 21:22:09 -0400

    fix lua

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ git clone https://github.com/panux/lpkg.git
 echo $ROOTFS
 chmod 700 lpkg/lpkg.lua
 #lpkg/lpkg.lua bootstrap $(pwd)/build-x86 https://repo.projectpanux.com/beta/x86/pkgs/
-lua5.3 lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86_64 "$(pwd)/build-x86_64" || exit 1
+lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86_64 "$(pwd)/build-x86_64" || exit 1
 
 #mv build-x86 build
 #docker build -t panux/panux:x86 .
