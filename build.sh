7caa25348dd358a0dd01086e1c4b86573ef7d067
Jaden Weiss <jadr2ddude@gmail.com>
2017-09-15 16:45:59 -0400

    create users in container

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ echo $ROOTFS
 chmod 700 lpkg/lpkg.lua
 #lpkg/lpkg.lua bootstrap $(pwd)/build-x86 https://repo.projectpanux.com/beta/x86/pkgs/
 lua lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86_64 "$(pwd)/build-x86_64" || exit 1
+echo root:x:0:0:root:/root:/bin/sh > build-x86_64/etc/passwd
+echo root::0:0:99999:7::: > build-x86_64/etc/shadow
 
 #mv build-x86 build
 #docker build -t panux/panux:x86 .
