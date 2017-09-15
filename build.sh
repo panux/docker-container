ef5ca7f9f0becb1aa0eca277c1b65307c22dc840
Jaden Weiss <jadr2ddude@gmail.com>
2017-09-15 17:13:08 -0400

    set up /etc/group

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ chmod 700 lpkg/lpkg.lua
 lua lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86_64 "$(pwd)/build-x86_64" || exit 1
 echo root:x:0:0:root:/root:/bin/sh > build-x86_64/etc/passwd
 echo root::0:0:99999:7::: > build-x86_64/etc/shadow
+echo root::0:root > build-x86_64/etc/group
 
 #mv build-x86 build
 #docker build -t panux/panux:x86 .
