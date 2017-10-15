28bee23a3eb431ac0d1d862d9bf92d55d1476a54
Jaden Weiss <jadr2ddude@gmail.com>
2017-10-14 23:42:38 -0400

    re-enable x86

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 git clone https://github.com/panux/lpkg.git
 echo $ROOTFS
 chmod 700 lpkg/lpkg.lua
-#lpkg/lpkg.lua bootstrap $(pwd)/build-x86 https://repo.projectpanux.com/beta/x86/pkgs/
+lua lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86 "$(pwd)/build-x86" || exit 1
 lua lpkg/lpkg.lua bootstrap "repo.projectpanux.com" beta x86_64 "$(pwd)/build-x86_64" || exit 1
 echo root:x:0:0:root:/root:/bin/sh > build-x86_64/etc/passwd
 echo root::0:0:99999:7::: > build-x86_64/etc/shadow
 echo root::0:root > build-x86_64/etc/group
+echo root:x:0:0:root:/root:/bin/sh > build-x86/etc/passwd
+echo root::0:0:99999:7::: > build-x86/etc/shadow
+echo root::0:root > build-x86/etc/group
 
-#mv build-x86 build
-#docker build -t panux/panux:x86 .
-#mv build build-x86
+mv build-x86 build
+docker build -t panux/panux:x86 . || exit 1
+mv build build-x86
 
 mv build-x86_64 build
-docker build -t panux/panux:x86_64 .
+docker build -t panux/panux:x86_64 . || exit 1
 mv build build-x86_64
